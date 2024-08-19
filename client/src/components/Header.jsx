334eb4f5a2c1ed1afd774a8a323576b7a3f54dd1
Mohammad-Rasel <mrhrasel229@gmail.com>
2024-08-19 23:13:18 +0600

    add redux toolkit and store

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { FaSearch } from "react-icons/fa"; 
 // import { BiSearch } from "react-icons/bi"; 
 // import { AiOutlineSearch } from "react-icons/ai"; 
-import React from 'react'
+// import React from 'react'
 import { Link } from "react-router-dom";
 export default function Header() {
   return (
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+// import React from 'react'
 import { Link , useNavigate} from 'react-router-dom'
 import { useState } from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice';
 export default function Signin() {
   const [formData, setFormData]= useState({}); 
-  const [error, setError]= useState(null); 
-  const [loading, setLoading]= useState(false); 
+  // const [error, setError]= useState(null); 
+  // const [loading, setLoading]= useState(false); 
+  const {error, loading}= useSelector((state)=> state.user)
   const navigate= useNavigate(); 
+  const dispatch= useDispatch(); 
 
   const handleChange=(e)=>{
     setFormData ({
@@ -17,7 +20,7 @@ export default function Signin() {
    const handleSubmit=async (e)=>{
     e.preventDefault(); 
     try{
-      setLoading(true); 
+      dispatch(signInStart()); 
     // form validation herecons
     const res= await fetch('api/auth/sign-in', {
       method: 'POST',
@@ -26,17 +29,14 @@ export default function Signin() {
     });
     const data= await res.json(); 
     if(data.success===false){
-      setError(data.message); 
-      setLoading(false);
+     dispatch(signInFailure(data.message)); 
       return;
     }
-    setLoading(false); 
-    setError(null);
+    dispatch(signInSuccess(data));
     navigate('/'); 
   
     }catch(error){
-      setLoading(false); 
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   }
     
