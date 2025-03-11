2b8b597cf69b209b27cbe998ab71637bccf9a2bf
Mohammad-RaselR <mrhrasel229@gmail.com>
2025-03-11 16:56:31 +0600

    delete listing functionality api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.module.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing= async (req, res, next) => {
 
@@ -14,4 +15,21 @@ export const createListing= async (req, res, next) => {
 
     }
 
+}
+
+export const deleteListing= async (req, res, next) => {
+    const listing= await Listing.findById(req.params.id);
+    
+    if(!listing){
+        return next(errorHandler(404, "Listing not found"));
+    }
+    if(req.user.id !== listing.userRef.toString()){
+        return next(errorHandler(401, "Unauthorized"));
+    }
+    try{
+        await Listing.findByIdAndDelete(req.params.id);
+        return res.status(200).json({message: "Listing deleted"});
+    }catch(error){
+        next(errorHandler(500, "Internal Server Error"));
+    }
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -236,6 +236,7 @@ export default function Profile() {
               <button className="text-red-700 uppercase" onClick={()=> handleListingDelete(listing._id)}>Delete</button>
               <Link to={`/update-listing/${listing._id}`} >
                 <button className="text-green-700 uppercase">Edit</button>
+               
               </Link>
             </div>
 
